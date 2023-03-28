76b53d1279b828f8b6587ce99ee59780de121755
Igor Cerqueira <igor0911@hotmail.com>
2023-03-28 16:11:28 -0300

    atualização de background botões e outros ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule,
